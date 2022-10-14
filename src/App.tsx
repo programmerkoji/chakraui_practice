3fc7a535deef6171934ece8fb9e2e42c0dc6f578
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2022-10-14 09:00:12 +0900

    react,v17へダウングレード、chakrauiのインストール

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button, ChakraProvider } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Button colorScheme="teal">ボタン</Button>
+    </ChakraProvider>
+  );
+}
+
+export default App;
