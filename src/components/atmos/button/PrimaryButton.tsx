51898d33aacd48a78e9d5d2643abed7c86f65ee9
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2022-10-15 10:04:35 +0900

    ログイン機能の実装

--- src/components/atmos/button/PrimaryButton.tsx
+++ src/components/atmos/button/PrimaryButton.tsx
@@ -2,15 +2,25 @@ import { memo, ReactNode, VFC } from "react";
 import { Button } from "@chakra-ui/react";
 
 type Props = {
-  children: ReactNode;
+	children: ReactNode;
+	disabled?: boolean;
+	loading?: boolean;
+	onClick: () => void;
 };
 
 export const PrimaryButton: VFC<Props> = memo((props) => {
-	const { children } = props;
+	const { children, disabled = false, loading = false, onClick } = props;
 
 	return (
-		<Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
-      {children}
+		<Button
+			bg="teal.400"
+			color="white"
+			_hover={{ opacity: 0.8 }}
+			disabled={disabled || loading}
+			isLoading={loading} //chakraUIのローディング
+			onClick={onClick}
+		>
+			{children}
 		</Button>
 	);
 });
