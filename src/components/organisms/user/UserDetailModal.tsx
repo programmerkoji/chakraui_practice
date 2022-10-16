100b7a5cd5058082a53643ab5e62f938ce294fb7
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2022-10-16 09:56:43 +0900

    ユーザー詳細画面作成（モーダル）

---
+++ src/components/organisms/user/UserDetailModal.tsx
@@ -0,0 +1,57 @@
+import { memo, VFC } from "react";
+import {
+	FormControl,
+	FormLabel,
+	Input,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalHeader,
+	ModalOverlay,
+	Stack,
+} from "@chakra-ui/react";
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+export const UserDetailModal: VFC<Props> = memo((props) => {
+	const { isOpen, onClose } = props;
+
+	return (
+		<Modal
+			isOpen={isOpen}
+			onClose={onClose}
+			autoFocus={false}
+			motionPreset="slideInBottom"
+		>
+			<ModalOverlay />
+			<ModalContent pb={6}>
+				<ModalHeader>ユーザー詳細</ModalHeader>
+				<ModalCloseButton />
+				<ModalBody mx={4}>
+					<Stack spacing={4}>
+						<FormControl>
+							<FormLabel>名前</FormLabel>
+							<Input value="こーじ" isReadOnly />
+						</FormControl>
+						<FormControl>
+							<FormLabel>フルネーム</FormLabel>
+							<Input value="Koji Sato" isReadOnly />
+						</FormControl>
+						<FormControl>
+							<FormLabel>メール</FormLabel>
+							<Input value="test@test.com" isReadOnly />
+						</FormControl>
+						<FormControl>
+							<FormLabel>TEL</FormLabel>
+							<Input value="090-1234-5678" isReadOnly />
+						</FormControl>
+					</Stack>
+				</ModalBody>
+			</ModalContent>
+		</Modal>
+	);
+});
